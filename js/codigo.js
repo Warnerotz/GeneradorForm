be587bc4ce0d5770648d221b366788857500f465
warnerotz <adrichony@gmail.com>
2017-12-12 13:54:13 +0100

    asdf

--- js/codigo.js
+++ js/codigo.js
@@ -1,14 +1,23 @@
 $(document).ready(function (){
-  var arrayElementos =["Text Box", "Boton", "Text Area", "Radio Button", "Check Box"];
+  
+    $('input').iCheck({
+    checkboxClass: 'icheckbox_futurico',
+    radioClass: 'iradio_futurico',
+    increaseArea: '20%' // optional
+  });
+    
+    var arrayElementos =["Text Box", "Boton", "Text Area", "Radio Button", "Check Box"];
     
     
     
     
-    
+/*    
   function crearElementos(){
       for(var elemento)
       
       
       
   }
+  
+*/  
 });
