a1a0c44d22765e98f1ce1f7c47e0a15fdb15a7b5
warnerotz <adrichony@gmail.com>
2017-12-12 11:32:51 +0100

    asdf

---
+++ js/codigo.js
@@ -0,0 +1,14 @@
+$(document).ready(function (){
+  var arrayElementos =["Text Box", "Boton", "Text Area", "Radio Button", "Check Box"];
+    
+    
+    
+    
+    
+  function crearElementos(){
+      for(var elemento)
+      
+      
+      
+  }
+});
